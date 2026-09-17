da1e40cee404e4e6f0541f807886f4a327286a9f
saranrunran <peemai.saranporn@gmail.com>
2026-09-17 11:10:51 +0700

    pai gorn

--- src/middlewares/authenMiddleware.ts
+++ src/middlewares/authenMiddleware.ts
@@ -18,7 +18,6 @@ export const authenticateToken = (
     });
   }
 
-  // 2. extract the "...JWT-Token..." if available
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null)
     return res.status(401).json({
@@ -27,8 +26,6 @@ export const authenticateToken = (
     });
 
   try {
-    // 3. verify token using JWT_SECRET_KEY and
-    //    get payload "user" = { username, studentId, role }
     const jwt_secret = process.env.JWT_SECRET || "this_is_my_secret";
     jwt.verify(token, jwt_secret, (err, user) => {
       if (err)
@@ -37,11 +34,9 @@ export const authenticateToken = (
           message: "Invalid or expired token",
         });
 
-      // 4. Attach "user" payload and "other stuffs" to the custom request
       req.user = user as UserPayload;
       req.token = token;
 
-      // 5. Proceed to next middleware or route handler
       next();
     });
   } catch (err) {
--- src/routes/itemsRoutes.ts
+++ src/routes/itemsRoutes.ts
@@ -14,23 +14,34 @@ import { items } from "../db/db.js";
 //import uuid
 import { v4 as uuidv4 } from 'uuid';
 import { success } from "zod";
+import { users } from "../db/db.js";
+
+import type { User, UserPayload, CustomRequest } from "../libs/types.ts";
+import { authenticateToken } from "../middlewares/authenMiddleware.ts";
 
 const router = Router();
 
 // GET /api/vXXX/items/:userId 
-router.get("/:userId",(req: Request, res: Response) => {
-    try {
+router.get("/:userId", authenticateToken, (req: CustomRequest, res: Response) => {
+  try {
     const userId = req.params.userId;
     const parseResult = zUserId.safeParse(userId);
-
+    const token = req.query.authenticateToken;
+    
     if (!parseResult.success) {
       return res.status(400).json({
-        // message: "Validation failed",
-        // errors: parseResult.error.issues[0]?.message,
-        success: false,
-        message: "Forbidden access"
+        message: "Validation failed",
+        errors: parseResult.error.issues[0]?.message,
       });
     }
+
+    // หนูไม่แน่ใจกับเรื่องนี้ แต่ก็คือดึงโทเคนจากพารามมา แล้วก็เอาไปเช็คกับที่เรามีอยู่ว่าตรงกันไหม
+    // if(token !== userId) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "Forbidden access"
+    //   });
+    // }
 
     const itemFilter = items.filter(
       (i: Item) => i.userId === userId
